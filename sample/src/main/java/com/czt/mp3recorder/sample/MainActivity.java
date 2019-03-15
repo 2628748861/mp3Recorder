45a831b5b279955e3c613636e2e5f05369ce9051
2628748861 <2628748861@qq.com>
2019-03-15 14:50:00 +0800

    初始提交

--- sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
+++ sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
@@ -4,14 +4,19 @@ package com.czt.mp3recorder.sample;
 import java.io.File;
 import java.io.IOException;
 
+import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.czt.mp3recorder.MP3Recorder;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends Activity {
 
@@ -21,6 +26,17 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		new RxPermissions(this)
+				.request(Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+		.subscribe(new Consumer<Boolean>() {
+			@Override
+			public void accept(Boolean aBoolean) throws Exception {
+				Toast.makeText(MainActivity.this, "授权结果:"+aBoolean, Toast.LENGTH_SHORT).show();
+
+			}
+		});
+
 		Button startButton = (Button) findViewById(R.id.StartButton);
 		startButton.setOnClickListener(new OnClickListener() {
 			@Override
